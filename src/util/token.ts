45954f513e3cb3d27a7eff69117757852e3d8994
WMXPY <wm@mengw.io>
2019-04-21 00:46:40 -0500

    move controller

---
+++ src/util/token.ts
@@ -0,0 +1,14 @@
+/**
+ * @author WMXPY
+ * @namespace Brontosaurus_Mint_Util
+ * @description Token
+ */
+
+import { Basics } from "@brontosaurus/definition";
+import { _Map } from "@sudoo/bark/map";
+import { INFOS_SPLITTER } from "../interface/account";
+
+export const parseInfo = (infoRecord: Record<string, Basics>): string[] => {
+
+    return _Map.keys(infoRecord).map((key: string) => key + INFOS_SPLITTER + infoRecord[key]);
+};
