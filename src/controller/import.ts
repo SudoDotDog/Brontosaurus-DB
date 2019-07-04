b8fe0d6eb0de12d859b65af83eeb4801e1cac9f4
WMXPY <wm@mengw.io>
2019-07-04 12:54:35 -0500

    fix import

--- src/controller/import.ts
+++ src/controller/import.ts
@@ -6,10 +6,11 @@
 
 import * as AccountController from "./account";
 import * as ApplicationController from "./application";
+import * as DecoratorController from "./decorator";
 import * as GroupController from "./group";
 import * as InformationController from "./information";
 import * as OrganizationController from "./organization";
 import * as PreferenceController from "./preference";
 
-export { AccountController, ApplicationController, InformationController, GroupController, OrganizationController, PreferenceController };
+export { AccountController, ApplicationController, DecoratorController, InformationController, GroupController, OrganizationController, PreferenceController };
 
