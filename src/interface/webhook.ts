092b65bccae403e51e3b9cd52be9f20c3d28e642
WMXPY <wm@mengw.io>
2019-07-17 00:05:57 -0500

    implement webhook. interface model

---
+++ src/interface/webhook.ts
@@ -0,0 +1,35 @@
+/**
+ * @author WMXPY
+ * @namespace Brontosaurus_DB_Interface
+ * @description Webhook
+ */
+
+export enum WEBHOOK_EVENT {
+
+    ACCOUNT_DEACTIVATE = "ACCOUNT_DEACTIVATE",
+    ORGANIZATION_DEACTIVATE = "ORGANIZATION_DEACTIVATE",
+
+    ACCOUNT_CREATE = "ACCOUNT_CREATE",
+    ORGANIZATION_CREATE = "ORGANIZATION_CREATE",
+}
+
+export interface IWebhookConfig {
+
+    readonly anchor: string;
+    readonly name: string;
+
+    description?: string;
+
+    url: string;
+}
+
+export interface IWebhook extends IWebhookConfig {
+
+    hooks: WEBHOOK_EVENT[];
+
+    active: boolean;
+    history: string[];
+
+    readonly createdAt: Date;
+    readonly updatedAt: Date;
+}
---
+++ src/model/webhook.ts
@@ -0,0 +1,65 @@
+/**
+ * @author WMXPY
+ * @namespace Brontosaurus_DB_Model
+ * @description Webhook
+ */
+
+import { Document, model, Model, Schema } from "mongoose";
+import { IWebhook } from "../interface/webhook";
+
+const WebhookSchema: Schema = new Schema(
+    {
+        active: {
+            type: Boolean,
+            required: true,
+            default: true,
+        },
+        anchor: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true,
+        },
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+        hooks: {
+            type: [String],
+            required: true,
+            default: [],
+        },
+        history: {
+            type: [String],
+            required: true,
+            default: [],
+        },
+    }, {
+        timestamps: {
+            createdAt: true,
+            updatedAt: true,
+        },
+    },
+);
+
+export interface IWebhookModel extends IWebhook, Document {
+
+    pushHistory(history: string): IWebhookModel;
+}
+
+WebhookSchema.methods.pushHistory = function (this: IWebhookModel, history: string): IWebhookModel {
+
+    this.history = [...this.history, history];
+
+    return this;
+};
+
+export const WebhookModel: Model<IWebhookModel> = model<IWebhookModel>('Webhook', WebhookSchema);
