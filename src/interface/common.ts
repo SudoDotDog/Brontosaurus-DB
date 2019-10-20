abd15696e13d8252ae827d22f539cd3e9cb250ce
WMXPY <wm@mengw.io>
2019-10-20 17:25:05 -0500

    implement common model, interface

---
+++ src/interface/common.ts
@@ -0,0 +1,16 @@
+/**
+ * @author WMXPY
+ * @namespace Interface
+ * @description Common
+ */
+
+import { ObjectID } from "bson";
+
+export type History = {
+
+    readonly action: string;
+    readonly by: ObjectID;
+    readonly at: Date;
+    readonly content: string;
+    readonly extra?: any;
+};
---
+++ src/model/common.ts
@@ -0,0 +1,31 @@
+/**
+ * @author WMXPY
+ * @namespace Model
+ * @description Common
+ */
+
+import { Schema } from "mongoose";
+
+export const HistorySchema = new Schema({
+
+    action: {
+        type: String,
+        required: true,
+    },
+    by: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    at: {
+        type: Date,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    extra: {
+        type: Schema.Types.Mixed,
+        required: false,
+    },
+}, { _id: false });
