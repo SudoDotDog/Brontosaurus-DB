fff9a9f0c0bc67055a86474ddb22fc4588b52e62
WMXPY <wm@mengw.io>
2019-10-22 09:52:32 -0500

    fix account history

--- src/interface/account.ts
+++ src/interface/account.ts
@@ -14,6 +14,7 @@ export const defaultInitialAttemptPoints: number = 100;
 export type AccountActions = {
 
     CREATE: undefined,
+    RESET_PASSWORD: undefined,
 };
 
 export interface IAccountConfig {
