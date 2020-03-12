710133efceb50d0869eede61c4dddd3f11af14a6
WMXPY <wm@mengw.io>
2020-03-12 16:15:07 -0500

    add match account by namespace route

--- src/controller/match.ts
+++ src/controller/match.ts
@@ -8,7 +8,7 @@ import { AccountNamespaceMatch, MATCH_FAILS_REASON } from "../data/match";
 import { IAccountModel } from "../model/account";
 import { INamespaceModel } from "../model/namespace";
 import { getAccountByUsernameAndNamespace } from "./account";
-import { getNamespaceByNamespace } from "./namespace";
+import { getNamespaceByDomain, getNamespaceByNamespace } from "./namespace";
 
 export const getAccountNamespaceMatchByUsernameAndNamespace = async (username: string, namespace: string): Promise<AccountNamespaceMatch> => {
 
@@ -34,4 +34,26 @@ export const getAccountNamespaceMatchByUsernameAndNamespace = async (username: s
         account,
         namespace: namespaceInstance,
     };
+};
+
+export const getAccountByUsernameAndNamespaceName = async (username: string, namespace: string): Promise<IAccountModel | null> => {
+
+    const namespaceInstance: INamespaceModel | null = await getNamespaceByNamespace(namespace);
+    if (!namespaceInstance) {
+        return null;
+    }
+
+    const account: IAccountModel | null = await getAccountByUsernameAndNamespace(username, namespaceInstance._id);
+    return account;
+};
+
+export const getAccountByUsernameAndNamespaceDomain = async (username: string, domain: string): Promise<IAccountModel | null> => {
+
+    const namespaceInstance: INamespaceModel | null = await getNamespaceByDomain(domain);
+    if (!namespaceInstance) {
+        return null;
+    }
+
+    const account: IAccountModel | null = await getAccountByUsernameAndNamespace(username, namespaceInstance._id);
+    return account;
 };
