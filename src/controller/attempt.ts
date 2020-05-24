e35762db7faab3f0450de51503b47e6f9e2f01d7
WMXPY <wm@mengw.io>
2020-05-24 17:32:24 -0500

    implement service attempt

--- src/controller/attempt.ts
+++ src/controller/attempt.ts
@@ -4,16 +4,37 @@
  * @description Attempt
  */
 
+import { randomUnique } from "@sudoo/random";
 import { ObjectID } from "bson";
 import { IAttemptConfig } from "../interface/attempt";
 import { AttemptModel, IAttemptModel } from "../model/attempt";
 
 export const createUnsavedAttempt = (config: IAttemptConfig): IAttemptModel => {
 
+    const identifier: string = randomUnique();
     return new AttemptModel({
         ...config,
         at: new Date(),
+        identifier,
     });
+};
+
+export const getAttemptById = async (attemptId: ObjectID | string): Promise<IAttemptModel | null> => {
+
+    const attempt: IAttemptModel | null = await AttemptModel.findOne({
+        _id: attemptId,
+    });
+
+    return attempt;
+};
+
+export const getAttemptByAttemptIdentifier = async (identifier: string): Promise<IAttemptModel | null> => {
+
+    const attempt: IAttemptModel | null = await AttemptModel.findOne({
+        identifier,
+    });
+
+    return attempt;
 };
 
 export const getAttemptsByAccount = async (account: ObjectID | string): Promise<IAttemptModel[]> => {
